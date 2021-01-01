60cd1e51ae870b82bdbd4933866ebadc5513e01e
Khan Mohsin <m90khan@gmail.com>
2021-01-02 04:22:56 +0500

    resolvers and trivial obj types

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -2,6 +2,9 @@ import { MongoClient } from 'mongodb';
 import { Database } from '../lib/types';
 // Connection URL
 const DB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net`;
+/*
+connectDatabase is a async func so we have to use Promise<Database> to implement types
+*/
 export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(DB, {
     useNewUrlParser: true,
--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -3,10 +3,10 @@ import { ObjectId } from 'mongodb';
 
 // Iresolvers  help defined types of resolver map
 import { IResolvers } from 'apollo-server-express';
-import { Database, Listing } from '../lib/types';
+import { Database, Listing } from '../../../lib/types';
 //  resolvers: just a map of object tthat relate with schema field that resolve that field
 
-export const resolvers: IResolvers = {
+export const listingResolvers: IResolvers = {
   Query: {
     listings: async (
       _root: undefined,
@@ -27,6 +27,7 @@ root:   The object returned from the resolver on the parent ﬁeld. set to undef
       { id }: { id: string },
       { db }: { db: Database }
     ): Promise<Listing> => {
+      // filter by Object _id from database by converting the string id to hex number
       const deleteRes = await db.listings.findOneAndDelete({ _id: new ObjectId(id) });
       if (!deleteRes.value) {
         throw new Error('failed to delete listing');
@@ -34,7 +35,18 @@ root:   The object returned from the resolver on the parent ﬁeld. set to undef
       return deleteRes.value;
     },
   },
+  /* 
+  resolve Listing Object type: the return values from QUery and mutation 
+   are the object argument for the children fields.
+   assign the Listing type from the return listing obj from the databse 
+   Resolver functions that simply return a property from the resolver obj argument with the same name as the
+ﬁeld being queried.
+   - Apollo server allows the simple resolve functions to omit defining them where the 
+   - object return the same value like title:(listing: Listing) = > listing.title 
+   * but id field has to resolved as id is an objectID needed as string
+*/
   Listing: {
+    // id gets seralized as string in graphql type obj
     id: (listing: Listing): string => listing._id.toString(),
   },
 };
