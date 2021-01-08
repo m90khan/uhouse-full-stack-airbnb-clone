4d1a75e9c46abd56d5fac9100f94cd9276d4fb24
Khan Mohsin <m90khan@gmail.com>
2021-01-08 21:59:34 +0500

    geocode

--- server/src/lib/api/Google.ts
+++ server/src/lib/api/Google.ts
@@ -1,4 +1,29 @@
 import { google } from 'googleapis';
+import { createClient, AddressComponent } from '@google/maps';
+
+const maps = createClient({ key: `${process.env.G_GEOCODE_KEY}`, Promise });
+
+const parseAddress = (addressComponents: AddressComponent<any>[]) => {
+  let country = null;
+  let admin = null;
+  let city = null;
+
+  for (const component of addressComponents) {
+    if (component.types.includes('country')) {
+      country = component.long_name;
+    }
+
+    if (component.types.includes('administrative_area_level_1')) {
+      admin = component.long_name;
+    }
+
+    if (component.types.includes('locality') || component.types.includes('postal_town')) {
+      city = component.long_name;
+    }
+  }
+
+  return { country, admin, city };
+};
 
 /*
 1- configure auth object
@@ -41,5 +66,14 @@ export const Google = {
       personFields: 'emailAddresses,names,photos',
     });
     return { user: data };
+  },
+  geocode: async (address: string) => {
+    const res = await maps.geocode({ address }).asPromise();
+
+    if (res.status < 200 || res.status > 299) {
+      throw new Error('failed to geocode address');
+    }
+
+    return parseAddress(res.json.results[0].address_components);
   },
 };
